1b53b9dd4ce14c36211603d5e269a85b9f40b267
aayushker <singhaayushker@gmail.com>
2024-07-23 19:26:32 +0530

    Completed Recipe info

--- frontend/app/components/AddRecipe/sections/RecipeInfo.tsx
+++ frontend/app/components/AddRecipe/sections/RecipeInfo.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-import { Textarea } from "@nextui-org/react";
+import { Input, Textarea, TimeInput } from "@nextui-org/react";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@nextui-org/react";
+import { set } from "jodit/types/core/helpers";
 
 const RecipeInfo = () => {
+  const [selectedKeys, setSelectedKeys] = React.useState(new Set(["select"]));
+
+  const selectedValue = React.useMemo(
+    () => Array.from(selectedKeys).join(", ").replaceAll("_", " "),
+    [selectedKeys]
+  );
+
   return (
     <>
       <p className="text-black text-4xl font-semibold drop-shadow-md bg-clip-text animate-gradient">
@@ -14,12 +29,89 @@ const RecipeInfo = () => {
       <Textarea
         isRequired
         label="Recipe Name"
-        labelPlacement="outside"
+        labelPlacement="outside-left"
         placeholder="Enter a tasty name for your recipe"
-        className="max-w-xs"
+        className="max-w-md p-6"
+        minRows={1}
+        maxRows={1}
       />
 
-      
+      <Textarea
+        isRequired
+        label="Description"
+        labelPlacement="outside-left"
+        placeholder="Here, you can add a short and sweet catchy summary for your recipes to grab your readers attention."
+        className="max-w-lg p-6"
+        minRows={5}
+      />
+
+      <Input
+        isRequired
+        type="number"
+        label="Total Time"
+        placeholder="0"
+        labelPlacement="outside-left"
+        startContent={
+          <div className="pointer-events-none flex items-center">
+            <span className="text-default-400 text-small">Minutes</span>
+          </div>
+        }
+        maxLength={1}
+        width={1}
+        className="max-w-xs p-6"
+        description="Please add the total time in minutes (Required)"
+      />
+
+      <Input
+        type="number"
+        label="Total Calories"
+        placeholder="0"
+        labelPlacement="outside-left"
+        startContent={
+          <div className="pointer-events-none flex items-center">
+            <span className="text-default-400 text-small">Calories</span>
+          </div>
+        }
+        maxLength={1}
+        width={1}
+        className="max-w-xs p-6"
+        description="Please add the total calories (Optional)"
+      />
+
+      <Input
+        isRequired
+        type="number"
+        label="Servings"
+        placeholder="0"
+        labelPlacement="outside-left"
+        maxLength={1}
+        width={1}
+        className="max-w-xs p-6"
+        description="Please tell the total servings (Required)"
+      />
+
+      <p>Difficulty Level</p>
+      <div className="px-16 py-2">
+        <Dropdown className=" w-10 " dir="top-left">
+          <DropdownTrigger>
+            <Button variant="bordered" className="capitalize">
+              {selectedValue}
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            aria-label="Single selection example"
+            variant="flat"
+            disallowEmptySelection
+            selectionMode="single"
+            selectedKeys={selectedKeys}
+            onSelectionChange={setSelectedKeys}
+          >
+            <DropdownItem key="Beginner">Beginner</DropdownItem>
+            <DropdownItem key="Intermediate">Intermediate</DropdownItem>
+            <DropdownItem key="Advanced">Advanced</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
     </>
   );
 };
--- frontend/pages/addRecipe/index.tsx
+++ frontend/pages/addRecipe/index.tsx
@@ -8,6 +8,7 @@ const index = () => {
   return (
     <div className="">
       {/* <AddRecipeNavBar /> */}
+
       <AddRecipeSidebar />
     </div>
   );
