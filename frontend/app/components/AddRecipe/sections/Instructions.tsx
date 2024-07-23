abfc7695919ffeeb8e187bd430b6cb38c30132e0
aayushker <singhaayushker@gmail.com>
2024-07-23 22:53:14 +0530

    Completed the Instruction section

--- frontend/app/components/AddRecipe/sections/Instructions.tsx
+++ frontend/app/components/AddRecipe/sections/Instructions.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState } from "react";
+import dynamic from 'next/dynamic';
+const QuillWrapper = dynamic(() => import('../ui/QuillWrapper'), { ssr: false });
 
 const Instructions = () => {
-  return (
-    <div>
-      Instructions
-    </div>
-  )
-}
 
-export default Instructions
+
+  const [content, setContent] = useState<string>('');
+
+  return (
+    <>
+      <p className="text-black text-2xl font-semibold drop-shadow-md bg-clip-text animate-gradient">
+        Instructions 📄
+      </p>
+      <p className="text-black text-md drop-shadow-md">
+        Tell us the sorcery behind your recipe! 🧙‍♂️
+      </p>
+      <QuillWrapper placeholder="Use your creativity to create a well written instructions section-" value={content} onChange={setContent} />
+    </>
+  );
+};
+
+export default Instructions;
--- frontend/app/components/AddRecipe/ui/QuillWrapper.tsx
+++ frontend/app/components/AddRecipe/ui/QuillWrapper.tsx
@@ -1,9 +1,9 @@
-import dynamic from 'next/dynamic';
-import React, { useState } from 'react';
+import dynamic from "next/dynamic";
+import React, { useState } from "react";
 
 // Dynamically import ReactQuill with SSR disabled
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
-import 'react-quill/dist/quill.snow.css';
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
 
 interface QuillWrapperProps {
   placeholder?: string;
@@ -11,15 +11,22 @@ interface QuillWrapperProps {
   onChange: (value: string) => void;
 }
 
-const QuillWrapper: React.FC<QuillWrapperProps> = ({ placeholder, value, onChange }) => {
+const QuillWrapper: React.FC<QuillWrapperProps> = ({
+  placeholder,
+  value,
+  onChange,
+}) => {
   const modules = {
     toolbar: [
-      [{ header: '1' }, { header: '2' }, { font: [] }],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      ['bold', 'italic', 'underline'],
-      ['link'],
-      ['clean']
-    ]
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ size: ["small", false, "large", "huge"] }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["bold", "italic", "underline"],
+      [{ 'color': [] }, { 'background': [] }], 
+      ["link"],
+      ["clean"],    
+      [{ 'align': [] }],
+    ],
   };
 
   return (
@@ -27,7 +34,7 @@ const QuillWrapper: React.FC<QuillWrapperProps> = ({ placeholder, value, onChang
       value={value}
       onChange={onChange}
       modules={modules}
-      placeholder={placeholder || 'Start typing...'}
+      placeholder={placeholder || "Start typing..."}
     />
   );
 };
