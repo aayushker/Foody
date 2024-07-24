19dad889abcfa9ce009d56254ba89ab5d5c406c1
aayushker <singhaayushker@gmail.com>
2024-07-24 23:30:30 +0530

    Created views for Recipes

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Recipe, Ingredient, Instruction, NutritionalInfo, Comment
+from .serializers import RecipeSerializer, IngredientSerializer, InstructionSerializer, NutritionalInfoSerializer, CommentSerializer
 
 # Create your views here.
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+class InstructionViewSet(viewsets.ModelViewSet):
+    queryset = Instruction.objects.all()
+    serializer_class = InstructionSerializer
+
+class NutritionalInfoViewSet(viewsets.ModelViewSet):
+    queryset = NutritionalInfo.objects.all()
+    serializer_class = NutritionalInfoSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
