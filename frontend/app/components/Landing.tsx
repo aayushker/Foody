a6c312b2f688df9f390c992f7201e517d2864674
aayushker <singhaayushker@gmail.com>
2024-08-24 22:24:13 +0530

    Added the FAQs section to the landing page

--- frontend/app/components/Landing.tsx
+++ frontend/app/components/Landing.tsx
@@ -3,12 +3,14 @@ import NavBar from "@/app/components/ui/NavBar";
 import Footer from "@/app/components/ui/Footer";
 import Hero from "@/app/components/Landing/Hero";
 import Contact from "@/app/components/Landing/Contact";
+import FAQs from "@/app/components/Landing/FAQs";
 
 const Landing = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <FAQs />
       <Contact />
       <Footer />
     </>
--- frontend/app/components/Landing/FAQs.tsx
+++ frontend/app/components/Landing/FAQs.tsx
@@ -1,25 +1,77 @@
 import React from "react";
-import { Accordion, AccordionItem } from "@nextui-org/react";
+import {
+  Accordion,
+  AccordionItem,
+  Card,
+  Divider,
+  Image,
+} from "@nextui-org/react";
 
 const FAQs = () => {
   return (
-    <Accordion variant="splitted">
-      <AccordionItem key="1" aria-label="What is this platform about?" title="What is this platform about?">
-        This platform is designed to help users discover, share, and create amazing recipes from around the world. It's a community-driven space where culinary enthusiasts can connect and learn from one another.
-      </AccordionItem>
-      <AccordionItem key="2" aria-label="How do I submit my own recipes?" title="How do I submit my own recipes?">
-        You can submit your own recipes by creating an account and navigating to the 'Add Recipe' section on your dashboard. Fill in the necessary details, upload images, and hit submit. Your recipe will be visible to the community once it's approved.
-      </AccordionItem>
-      <AccordionItem key="3" aria-label="Is there a way to save my favorite recipes?" title="Is there a way to save my favorite recipes?">
-        Yes, you can save your favorite recipes by clicking the 'Save' button on any recipe page. You can view all your saved recipes in the 'Favorites' section of your profile.
-      </AccordionItem>
-      <AccordionItem key="4" aria-label="Can I edit or delete my recipes after submission?" title="Can I edit or delete my recipes after submission?">
-        Absolutely! You can edit or delete your recipes by going to the 'My Recipes' section in your dashboard. Just click on the recipe you want to modify and make the necessary changes.
-      </AccordionItem>
-      <AccordionItem key="5" aria-label="How do I report a problem with a recipe?" title="How do I report a problem with a recipe?">
-        If you encounter an issue with a recipe, such as incorrect information or inappropriate content, you can report it by clicking the 'Report' button on the recipe page. Our team will review the report and take appropriate action.
-      </AccordionItem>
-    </Accordion>
+    <>
+      <Card fullWidth className="max-w-screen-xl mx-auto p-6 my-12">
+        <div className="flex flex-col lg:flex-row items-center lg:items-center">
+          <Divider orientation="vertical" className="hidden lg:block mx-4" />
+
+          <div className="w-full lg:w-1/2 max-w-md p-4">
+            <Accordion variant="splitted">
+              <AccordionItem
+                key="1"
+                aria-label="What is this platform about?"
+                title="What is this platform about?"
+              >
+                This platform is designed to help users discover, share, and
+                create amazing recipes from around the world. It's a
+                community-driven space where culinary enthusiasts can connect
+                and learn from one another.
+              </AccordionItem>
+              <AccordionItem
+                key="2"
+                aria-label="How do I submit my own recipes?"
+                title="How do I submit my own recipes?"
+              >
+                You can submit your own recipes by creating an account and
+                navigating to the 'Add Recipe' section on your dashboard. Fill
+                in the necessary details, upload images, and hit submit. Your
+                recipe will be visible to the community once it's approved.
+              </AccordionItem>
+              <AccordionItem
+                key="3"
+                aria-label="Is there a way to save my favorite recipes?"
+                title="Is there a way to save my favorite recipes?"
+              >
+                Yes, you can save your favorite recipes by clicking the 'Save'
+                button on any recipe page. You can view all your saved recipes
+                in the 'Favorites' section of your profile.
+              </AccordionItem>
+              <AccordionItem
+                key="4"
+                aria-label="Can I edit or delete my recipes after submission?"
+                title="Can I edit or delete my recipes after submission?"
+              >
+                Absolutely! You can edit or delete your recipes by going to the
+                'My Recipes' section in your dashboard. Just click on the recipe
+                you want to modify and make the necessary changes.
+              </AccordionItem>
+              <AccordionItem
+                key="5"
+                aria-label="How do I report a problem with a recipe?"
+                title="How do I report a problem with a recipe?"
+              >
+                If you encounter an issue with a recipe, such as incorrect
+                information or inappropriate content, you can report it by
+                clicking the 'Report' button on the recipe page. Our team will
+                review the report and take appropriate action.
+              </AccordionItem>
+            </Accordion>
+          </div>
+          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end p-4">
+            <Image src="/images/faqs.jpg" height={400} />
+          </div>
+        </div>
+      </Card>
+    </>
   );
 };
 
