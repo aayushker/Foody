05698e0072f8a81125bb183fa0f6752681bfb91d
aayushker <singhaayushker@gmail.com>
2025-01-04 22:49:31 +0530

    fix: Add media URL handling to urlpatterns in Django backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urls)),
     path('', Heartbeat, name='heartbeat')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
